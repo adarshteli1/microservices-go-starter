39cd47b611544e974a01c1bda8baf6a3349142ad
Addy <addy@kali.kali>
2026-06-08 04:51:43 -0400

    Add trip service and API gateway updates

---
+++ services/api-gateway/http.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"ride-sharing/shared/contracts"
+)
+
+func handleTripPreview(w http.ResponseWriter, r *http.Request) {
+	var reqBody previewTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	//validate
+
+	if reqBody.UserID == "" {
+		http.Error(w, "UserId is Required", http.StatusBadRequest)
+		return
+	}
+
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
+	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var respBody any
+	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
+		return
+	}
+
+	response := contracts.APIResponse{Data: respBody}
+
+	writeJson(w, http.StatusCreated, response)
+}
